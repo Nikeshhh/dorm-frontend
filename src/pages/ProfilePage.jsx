2d4ba2d98faf9329e681f2f9eb57e537c4d8bad0
Nikeshhh <WityazKajit@gmail.com>
2024-05-13 11:17:14 +0300

    styled profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import api from "../Api"
 import Loader from '../components/Loader.jsx'
-import { Button } from "@mui/material"
+import { Button, Typography } from "@mui/material"
 import LogOutModal from "../components/LogOutModal.jsx"
 
 
@@ -24,7 +24,33 @@ const ProfilePage = () => {
 
     return (
         <>
-            {userData ? (<div>
+        {userData ? 
+            (<div className="flex flex-col justify-center items-center" style={{ height: '80vh' }}>
+                <div className="p-4">
+                    <Typography variant="h6" gutterBottom>
+                        Имя пользователя: {userData.username}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                        Фамилия: {userData.surname}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                        Имя: {userData.name}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                        Отчество: {userData.second_name}
+                    </Typography>
+                    <div className="mt-4">
+                        <Button onClick={handleOpen} variant="contained" color="primary" fullWidth>
+                            Выйти
+                        </Button>
+                    </div>
+                </div>
+                <LogOutModal open={open} handleClose={handleClose} />
+            </div>)
+            :
+            <Loader />
+}
+            {/* {userData ? (<div>
             <p>{userData.username}</p>
             <p>{userData.surname}</p>
             <p>{userData.name}</p>
@@ -32,7 +58,7 @@ const ProfilePage = () => {
             <Button onClick={handleOpen}>Выйти</Button>
             <LogOutModal open={open} handleClose={handleClose} />
             </div>
-            ) : <Loader /> }
+            ) : <Loader /> } */}
         </>
     )
 }
